024dab71bbb83214a2e7be700c3b08f887cede59
jaychotaliya987 <jaychotaliya987@gmail.com>
2025-01-07 17:58:29 +0100

    Diagram And Echostate Init

--- ReservoirGrid/Examples/learning.py
+++ ReservoirGrid/Examples/learning.py
@@ -9,18 +9,15 @@ sys.path.append(os.path.abspath(os.path.join(__file__, '..', '..')))
 from Datasets.MackeyGlass2DDataset import MackeyGlass2DDataset
 from Datasets.MackeyGlassDataset import MackeyGlassDataset
 from Datasets.MackeyGlass import MackeyGlass
+from Reservoir import Echostate
 
+from torch.utils.data import DataLoader
 
 
 print("Imports Done!")
 
 Mglass1 = MackeyGlass2DDataset(20, 100, 17, 1)
-Mglass2 = MackeyGlassDataset(20, 100)
 
-
-
-print(Mglass1.__len__())
-print(Mglass1.__getitem__(1))
 
 
 
---
+++ ReservoirGrid/Reservoir/Echostate.py
@@ -0,0 +1,21 @@
+import numpy 
+import matplotlib.pyplot as plt
+
+import torch
+from torch import nn
+from torch import optim
+
+from torch.utils.data import dataset
+from torch.utils.data import DataLoader
+
+
+
+class EchoStates(nn.RNN):
+    
+    pass
+
+
+
+
+
+print("Echo State Module Loaded")
