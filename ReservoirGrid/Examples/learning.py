092c25a6085b2e7f1b2fd072325f66c6f30d9ad3
jaychotaliya987 <jaychotaliya987@gmail.com>
2025-01-07 14:52:31 +0100

    Mackey Glass

---
+++ ReservoirGrid/Examples/learning.py
@@ -0,0 +1,22 @@
+import torch
+import numpy as np
+import matplotlib.pyplot as plt
+import sys
+import os
+
+# Ensure the correct path to MackeyGlass module
+sys.path.append(os.path.abspath(os.path.join(__file__, '..', '..')))
+from Datasets.MackeyGlass2DDataset import MackeyGlass2DDataset
+from Datasets.MackeyGlassDataset import MackeyGlassDataset
+from Datasets.MackeyGlass import MackeyGlass
+
+
+
+print("Imports Done!")
+
+Mglass1 = MackeyGlass2DDataset(20, 100, 0.1, 1)
+Mglass2 = MackeyGlassDataset(20, 100)
+
+
+print(Mglass2.__len__())
+print(Mglass2.__getitem__(1))
