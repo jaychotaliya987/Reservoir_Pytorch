f432a4bcafe59e516d2dc22920374396199d3429
jaychotaliya987 <jaychotaliya987@gmail.com>
2025-01-15 17:10:02 +0100

    cutting it short today beacuse my lund friends came

--- ReservoirGrid/Reservoir/Echostate.py
+++ ReservoirGrid/Reservoir/Echostate.py
@@ -1,19 +1,43 @@
-import numpy 
+import math
+import numpy as np
 import matplotlib.pyplot as plt
-
 import torch
 from torch import nn
 from torch import optim
-
-from torch.utils.data import dataset
-from torch.utils.data import DataLoader
-
-
+from torch.utils.data import DataLoader, Dataset
 
 class EchoStates(nn.RNN):
-    
-    pass
+    def __init__(self, input_size, hidden_size, num_layers=1, batch_first=False):
+        super(EchoStates, self).__init__(input_size, hidden_size, 
+                                         num_layers=num_layers, 
+                                         batch_first=batch_first, 
+                                         nonlinearity='tanh')
+        
+        self.hidden_size = hidden_size
+        self.num_layers = num_layers
+        self.batch_first = batch_first
 
+        # Customize the initialization of the recurrent weights
+        self.reset_parameters()
+
+    def reset_parameters(self):
+        for name, param in self.named_parameters():
+            if 'weight_ih' in name:
+                nn.init.kaiming_uniform_(param, a=math.sqrt(5))
+            elif 'weight_hh' in name:
+                nn.init.orthogonal_(param)
+            elif 'bias' in name:
+                fan_in, _ = nn.init._calculate_fan_in_and_fan_out(param)
+                bound = 1 / math.sqrt(fan_in)
+                nn.init.uniform_(param, -bound, bound)
+
+    def forward(self, x, h_0=None):
+        if self.batch_first:
+            x = x.transpose(0, 1)
+        output, h_n = super(EchoStates, self).forward(x, h_0)
+        if self.batch_first:
+            output = output.transpose(0, 1)
+        return output, h_n
 
 print("Echo State Module Loaded")
 
