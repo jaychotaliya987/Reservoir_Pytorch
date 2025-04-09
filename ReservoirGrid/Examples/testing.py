07a77068e9997e345319e5d0283789891caa148a
Jay Chotaliya <jaychotaliya987@gmail.com>
2025-04-09 17:12:49 +0200

    Refactor reservoir model: update training and prediction methods, remove unused ESN class

--- ReservoirGrid/Examples/testing.py
+++ ReservoirGrid/Examples/testing.py
@@ -17,14 +17,14 @@ from Models.Reservoir import Reservoir
 print("Imports Done!\n")
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
-Mglass1 = MackeyGlassDataset(1000, 5, tau=17, seed=0)
+Mglass1 = MackeyGlassDataset(10000, 5, tau=17, seed=0)
 inputs, targets = Mglass1[0]
 inputs = inputs.to(device)
 targets = targets.to(device)
 
 # Create reservoir
 reservoir = Reservoir(input_dim=1, reservoir_dim=500, output_dim=1, 
-                     spectral_radius=0.95, leak_rate=0.3, sparsity=0.9)
+                     spectral_radius=0.90, leak_rate=0.3, sparsity=0.9)
 reservoir = reservoir.to(device)
 
 
@@ -32,11 +32,12 @@ reservoir = reservoir.to(device)
 reservoir.train_readout(inputs, targets, alpha=1e-6)
 
 # Predict future values
-steps = 200
+steps = 1000
 with torch.no_grad():
     initial_input = inputs[-1:] 
-    predictions = reservoir.predict(initial_input, steps=steps)
+    predictions = reservoir.predict(initial_input, steps=steps, teacher_forcing=None, warmup=0)
 predictions = predictions.squeeze(1).cpu().numpy()
+
 
 inputs_plot = inputs[:-steps].squeeze(1).cpu().numpy()  # (800,)
 true_future = targets[-steps:].squeeze(1).cpu().numpy()  # (200,)
@@ -60,8 +61,8 @@ plt.plot(range(len(inputs_plot), len(inputs_plot) + steps),
 # Reservoir Predictions (dashed with markers)
 plt.plot(range(len(inputs_plot), len(inputs_plot) + steps), 
          predictions, 
-         '--', color='#ff7f0e', linewidth=2,
-         marker='o', markersize=4, markevery=1,
+         color='#ff7f0e', linewidth=2,
+         marker='o', markersize=2, markevery=5,
          label="Reservoir Predictions")
 
 # Add vertical line at prediction start
@@ -70,7 +71,7 @@ plt.axvline(x=len(inputs_plot), color='gray', linestyle=':', alpha=0.7)
 # Add annotations
 plt.annotate('Prediction Start', 
              xy=(len(inputs_plot), np.min(inputs_plot)), 
-             xytext=(10, 100), textcoords='offset points',
+             xytext=(10, 10), textcoords='offset points',
              arrowprops=dict(arrowstyle="->"))
 
 # Formatting
