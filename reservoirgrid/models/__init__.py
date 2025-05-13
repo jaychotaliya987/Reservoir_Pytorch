a79d770d69008aab92d9a534ef3bc556982a6c2a
jaychotaliya987 <jaychotaliya987@gmail.com>
2025-05-13 14:02:24 +0200

    Generate a new CQOscRes- coupled quantum oscillator reservoir and refector old files, Added new example file for paper reproduction of 2023Quantum oscillator paper

--- reservoirgrid/models/__init__.py
+++ reservoirgrid/models/__init__.py
@@ -1,4 +1,5 @@
 from .Reservoir import Reservoir
 from .Reservoir_old import Reservoir_old
+from .CQOscRes import CQOscRes
 
-__ALL__ = ["Reservoir", "Reservoir_old"]
+__ALL__ = ["Reservoir", "Reservoir_old", "CQOscRes"]
