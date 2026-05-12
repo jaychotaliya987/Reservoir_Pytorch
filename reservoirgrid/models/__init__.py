8cd62d0d3ec553bb1babdd2e4dd12ee902cf2361
jay chotaliya <jaychotaliya987@gmail.com>
2026-05-12 16:37:35 +0200

    Removed old reservoir from init file

--- reservoirgrid/models/__init__.py
+++ reservoirgrid/models/__init__.py
@@ -1,5 +1,4 @@
 from .Reservoir import Reservoir
-from .Reservoir_old import Reservoir_old
 from .CQOscRes import CQOscRes
 
-__ALL__ = ["Reservoir", "Reservoir_old", "CQOscRes"]
+__ALL__ = ["Reservoir", "CQOscRes"]
