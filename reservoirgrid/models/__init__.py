c94ea354b2153ee6a9e577bd9f4dfbeaee7c49e4
jay chotaliya <jaychotaliya987@gmail.com>
2026-05-13 18:07:48 +0200

    merged batched reservoirs to handle everything

--- reservoirgrid/models/__init__.py
+++ reservoirgrid/models/__init__.py
@@ -1,7 +1,5 @@
 from .Reservoir import Reservoir
 from .CQOscRes import CQOscRes
-from .Reservoir_batched import Reservoir_batched
 
 __ALL__ = ["Reservoir", 
-           "CQOscRes",
-           "Reservoir_batched"]
+           "CQOscRes"]
