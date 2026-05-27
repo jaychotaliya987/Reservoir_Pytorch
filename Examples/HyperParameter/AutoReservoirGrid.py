6311cdbaa9f8a3d7e235ca5c8ff833b22f994e78
jay chotaliya <jaychotaliya987@gmail.com>
2026-05-27 17:18:03 +0200

    A Test Module for auto parameter tuning of reservoirgrid.reservoir

---
+++ Examples/HyperParameter/AutoReservoirGrid.py
@@ -0,0 +1,37 @@
+import os
+import sys
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '../..')))
+
+import torch
+import numpy as np
+from reservoirgrid.helpers import chaos_utils  
+from reservoirgrid.helpers import utils  
+from reservoirgrid.models.Reservoir import Reservoir
+from reservoirgrid.helpers import viz
+
+dataset = np.load("reservoirgrid/datasets/Chaotic/Lorenz.npy", allow_pickle=True)
+dataset = utils.normalize_data(dataset[15][1])
+
+X_train, X_val, Y_train, Y_val = utils.split(dataset) 
+
+model = Reservoir(input_dim=3, reservoir_dim=1000, output_dim=3)
+
+best_params = model.optimize(
+    X_train=X_train,
+    Y_train=Y_train,
+    X_val=X_val,
+    Y_val=Y_val,
+    metric_fn=chaos_utils.js_divergence, 
+    direction="minimize",
+    n_trials=5000,
+    batch_size=25,
+)
+
+predictions = model.predict(initial_input=Y_train, steps=len(Y_val))
+
+predictions = predictions.cpu().numpy()  
+Y_val = Y_val.cpu().numpy()  
+
+print(chaos_utils.js_divergence(Y_val, predictions))
+viz.compare_components([Y_val, predictions], labels=["True", "Predicted"], title="Optimized Reservoir Prediction").show()
+viz.compare_plot([Y_val, predictions], labels=["True", "Predicted"], title="Optimized Reservoir Prediction").show()
